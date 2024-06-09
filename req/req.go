061cd4401fcca3ba728efd204bb2f25ade9b7c04
lusqua <lucasaguiar.floripa@gmail.com>
2024-06-09 15:00:49 -0300

    feat. tidy request headers

--- req/req.go
+++ req/req.go
@@ -47,7 +47,7 @@ type SearchHistory struct {
 
 func MakeRequest(url string) (*http.Response, error) {
 	method := "POST"
-	payload := strings.NewReader("__aaid=0&__user=100006600013999&__a=1&__req=2&__hs=19870.BP%3ADEFAULT.2.0..0.0&dpr=2&__ccg=EXCELLENT&__rev=1013776097&__s=lhv54i%3Afh9gzd%3Ac704cs&__hsi=7373505932978384251&__dyn=7xe6Eiw_K9zo5ObwKBAgc9o2exu13wqojyUW3qi4EoxW4E7SewXwCwfW7oqx60Vo1upEK12wvk1bwbG78b87C2m3K2y11wBw5Zx62G3i1ywdl0Fw4Hwp8kwyx2cwAxq1yK1LwPxe3C0D8sKUbobEaUiyE725U4q0N8G0iS2S3qazo11E2XU6-1FwLw8O1pwr86C0gi1QwtU&__csr=&fb_dtsg=NAcNtXwpvpsQtRrysnMoxbQhOMpLs2_WJHEsk6a7v1Fv_pfKv2VJO5w%3A45%3A1700793422&jazoest=25818&lsd=qo0BXBOiVCYnSbBj9qT9LH&__spin_r=1013776097&__spin_b=trunk&__spin_t=1716778132&__jssesw=1")
+	payload := strings.NewReader("__a=1&fb_dtsg=NAcN8gzvxP02iYCYKB2u8DH4XGGSlImW_3_t2m-QqJuBVTENYAtnD0g%3A45%3A1700793422")
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
@@ -60,31 +60,8 @@ func MakeRequest(url string) (*http.Response, error) {
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
 	req.Header.Add(
 		"cookie",
-		"m_ls=%7B%22c%22%3A%7B%221%22%3A%22HCwAABbKwg0WlvqmuAgTBRbe4vOdk70tAA%22%2C%222%22%3A%22GSwVQBxMAAAWABb25t3GDBYAABV-HEwAABYAFoLn3cYMFgAAFigA%22%7D%2C%22d%22%3A%2204ada221-ff8c-4e34-858e-2290f771195c%22%2C%22s%22%3A%220%22%2C%22u%22%3A%22raydvk%22%7D; datr=agjkZMjFmfE67fxpgGx8H5sT; sb=8MQyZVYLK-FBdlP_IwJlMLQ6; c_user=100006600013999; usida=eyJ2ZXIiOjEsImlkIjoiQXNjMjE0bmhicWJydyIsInRpbWUiOjE3MTMzMDMwOTV9; ps_n=1; ps_l=1; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1713971290512%2C%22v%22%3A1%7D; dpr=1; xs=45%3AT_J_yRyfD1fbzA%3A2%3A1700793422%3A-1%3A10129%3A%3AAcV9pzWt35cKJ9r_ucSkBtF14Q_G_ewlWJZ4iTxw45w; fr=1XEVkxoj0gd0ovo1Q.AWU9GGO6GVPnI3diPd_Y_r7gsHM.BmU_SK..AAA.0.0.BmU_SK.AWVi7rhqAZc; wd=977x867",
+		"m_ls=%7B%22c%22%3A%7B%221%22%3A%22HCwAABbKwg0WlvqmuAgTBRbe4vOdk70tAA%22%2C%222%22%3A%22GSwVQBxMAAAWABb25t3GDBYAABV-HEwAABYAFoLn3cYMFgAAFigA%22%7D%2C%22d%22%3A%2204ada221-ff8c-4e34-858e-2290f771195c%22%2C%22s%22%3A%220%22%2C%22u%22%3A%22raydvk%22%7D; datr=agjkZMjFmfE67fxpgGx8H5sT; sb=8MQyZVYLK-FBdlP_IwJlMLQ6; c_user=100006600013999; ps_n=1; ps_l=1; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1716952782453%2C%22v%22%3A1%7D; xs=45%3AT_J_yRyfD1fbzA%3A2%3A1700793422%3A-1%3A10129%3A%3AAcWcg8lWgsb8GNTu0KEWT-sQgaRHHVlFTwKskf2FFSI; fr=1HTMXeLSSjpjSH1jR.AWVDExA0RUJiymUTauK4FqiKQbQ.BmYQ6K..AAA.0.0.BmYQ6K.AWVZcDSxEcM; usida=eyJ2ZXIiOjEsImlkIjoiQXNldHBpbWw5cDdlZiIsInRpbWUiOjE3MTc5NTM2NDZ9; wd=1472x857",
 	)
-	req.Header.Add("dnt", "1")
-	req.Header.Add("origin", "https://www.facebook.com")
-	req.Header.Add("priority", "u=1, i")
-	req.Header.Add(
-		"referer",
-		"https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=BR&q=%22software%20house%22&search_type=keyword_exact_phrase&media_type=all",
-	)
-	req.Header.Add("sec-ch-prefers-color-scheme", "dark")
-	req.Header.Add("sec-ch-ua", `"Chromium";v="125", "Not.A/Brand";v="24"`)
-	req.Header.Add("sec-ch-ua-full-version-list", `"Chromium";v="125.0.6422.112", "Not.A/Brand";v="24.0.0.0"`)
-	req.Header.Add("sec-ch-ua-mobile", "?0")
-	req.Header.Add("sec-ch-ua-model", `""`)
-	req.Header.Add("sec-ch-ua-platform", "macOS")
-	req.Header.Add("sec-ch-ua-platform-version", "14.4.1")
-	req.Header.Add("sec-fetch-dest", "empty")
-	req.Header.Add("sec-fetch-mode", "cors")
-	req.Header.Add("sec-fetch-site", "same-origin")
-	req.Header.Add(
-		"user-agent",
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-	)
-	req.Header.Add("x-asbd-id", "129477")
-	req.Header.Add("x-fb-lsd", "qo0BXBOiVCYnSbBj9qT9LH")
 
 	res, err := client.Do(req)
 	if err != nil {
@@ -121,7 +98,7 @@ func MakeUrl(search string) string {
 
 	searchquote := strings.Replace(search, " ", "%20", -1)
 
-	return baseurl + searchquote + "%22&v=550c25&session_id=8cd2ae4f-4ec2-4757-a6e5-416ff50ae254&count=30&active_status=all&ad_type=all&countries[0]=BR&media_type=all&search_type=keyword_exact_phrase"
+	return baseurl + searchquote + "%22&count=30&countries%5C[0%5C]=BR&search_type=keyword_exact_phrase"
 
 }
 
